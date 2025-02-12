1d4e91530c6991fc881afb206599cd0223dfb8af
rino09 <54140987+rino09@users.noreply.github.com>
2025-02-13 05:14:48 +0700

    Add files via upload

---
+++ pengaturan.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set("Asia/Jakarta");
+$conf_tanggal = 1; // 0 = MANUAL | 1 = Otomatis
+$tgl_manual = "02-04-2019"; // SILAHKAN GANTI TANGGAL JADIAN/ULTAH DIA
+$cowo = "Muhammad Reza Rizaldi"; // Nama Cowo Or Nama Lu
+$cewe = "Iqlima Oktaviani"; // Nama Cewe Lu | Kalo Jomblo Ya Makanya Punya Pacar Goblok
+if($conf_tanggal == "1"){
+    $skrng = date('d-m-Y');
+}else{
+    $skrng = $tgl_manual;
+}
+//$usernameIGcowo = "sayareja"; // Username IG LO
+//$usernameIGcewe = "iqlim"; // Username IG Cewe Lo
+$lagu = "kangen.mp3";
+/*
+SELAMAT BERKREASI 
+M Reza Rizaldi ♥ 1QLIMA OKTAVIANI
+*/
+?>
